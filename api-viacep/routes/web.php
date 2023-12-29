59439cf6d2c9b9795e8579da0b2da76a45782d22
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-12-29 08:50:54 -0300

    wip

--- api-viacep/routes/web.php
+++ api-viacep/routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
+use App\Livewire\SearchZipcode;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('/', function () {
-    // return view('welcome');
-    return 'toma';
-});
+Route::get('/', SearchZipcode::class)->name('search-zipcode');
