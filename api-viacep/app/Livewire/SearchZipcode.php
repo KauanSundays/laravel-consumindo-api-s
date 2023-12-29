7c4766737b81cb2eb8ba25b562d9b8f8fc0cfc92
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-12-29 09:02:24 -0300

    consuming api

--- api-viacep/app/Livewire/SearchZipcode.php
+++ api-viacep/app/Livewire/SearchZipcode.php
@@ -2,10 +2,19 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class SearchZipcode extends Component
 {
+    public $zipcodeData;
+
+    public function mount(): void
+    {
+        $response = Http::get('https://viacep.com.br/ws/05311020/json')->json();
+        dd($response);
+    }
+
     public function render()
     {
         return view('livewire.search-zipcode');
