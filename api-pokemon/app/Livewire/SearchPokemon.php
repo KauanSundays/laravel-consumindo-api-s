f42d7d1b1e6790a5d3e8f92b277f006bd12d9d18
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2024-01-02 17:05:06 -0300

    wip

--- api-pokemon/app/Livewire/SearchPokemon.php
+++ api-pokemon/app/Livewire/SearchPokemon.php
@@ -6,6 +6,29 @@ use Livewire\Component;
 
 class SearchPokemon extends Component
 {
+    // public $zipcodeData;
+    // public $zipcode = '';
+    // public $street = '';
+    // public $neighborhood = '';
+    // public $city = '';
+    // public $state = '';
+
+    // public function updatedZipcode(string $value) 
+    //pode ser colocado acim depois de updated, necessario adicionar o lazy no input
+    // {
+    //     dd($value);
+    // }
+
+    public function updatedZipcode(string $value)
+    {
+        $response = Http::get("https://viacep.com.br/ws/{$value}/json/")->json();
+
+        $this->street = $response['logradouro'] ?? '';
+        $this->neighborhood = $response['bairro'] ?? '';
+        $this->city = $response['localidade'] ?? '';
+        $this->state = $response['uf'] ?? '';
+    }
+
     public function render()
     {
         return view('livewire.search-pokemon');
